86b5a66582c0db6f49b2afa4219e1a6cdf49d59e
Yannick Aivayan <yannick.aivayan@visable.com>
2022-02-05 20:50:35 +0100

    style: minor corrections

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
 
 import "./MainLayout.css";
 import { ReactComponent as KazaLogo } from "../assets/kaza-logo.svg";
@@ -9,14 +9,14 @@ export default function MainLayout() {
     <div className="main-layout">
       <div className="main-layout__content">
         <header className="main-layout__header">
-          <NavLink to="/">
+          <Link to="/">
             <KazaLogo className="main-layout__header-logo" alt="kasa logo" />
-          </NavLink>
+          </Link>
           <nav className="main-layout__header-navbar">
             <NavLink to="/" className="main-layout__header-link">
               Accueil
             </NavLink>
-            <NavLink to="about" className="main-layout__header-link">
+            <NavLink to="/about" className="main-layout__header-link">
               À Propos
             </NavLink>
           </nav>
